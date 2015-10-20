be55d030c8af44e60718383473c3acfd06314675
Henning Vogt <henning.vogt@kaeuferportal.de>
2015-10-20 18:02:32 +0200

    Added basic Article actions and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,14 @@
 class ArticlesController < ApplicationController
+
   def new
+  end
+
+  def create
+    @article = Article.new(params[:article].permit(:title, :text))
+    @article.save
+    redirect_to @article
+  end
+
+  def show
   end
 end
