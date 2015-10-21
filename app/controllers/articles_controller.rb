71178ecf10310858501fad7219d63673c5e5bbec
Henning Vogt <henning.vogt@kaeuferportal.de>
2015-10-21 12:45:53 +0200

    Basic CRUD for articles done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,40 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params) ? (redirect_to @article) : (render 'new')
   end
 
   def create
-    @article = Article.new(params[:article].permit(:title, :text))
-    @article.save
-    redirect_to @article
+    @article = Article.new(article_params)
+    @article.save ? (redirect_to @article) : (render 'new')
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
